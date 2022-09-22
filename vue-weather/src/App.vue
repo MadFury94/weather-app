6b3cb18153d5abb2b75d8128c1a46e41f4149375
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-09-22 10:30:44 +0100

    first commit
    My first commit

---
+++ vue-weather/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
+
+    <RouterView />
+    <div></div>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/icons/SiteNavigation.vue";
+</script>
+
+<!-- <style>
+.page-enter-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from {
+  opacity: 0;
+}
+</style> -->
---
+++ vue-weather/src/components/icons/BaseModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <Transition name="modal-outer">
+    <div
+      v-show="modalActive"
+      class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+    >
+      <Transition name="modal-inner">
+        <div
+          v-if="modalActive"
+          class="p-4 bg-white self-start mt-32 max-w-screen-md"
+        >
+          <slot />
+          <button
+            class="text-white mt-8 bg-weather-primary py-2 px-6"
+            @click="$emit('close-modal')"
+          >
+            Close
+          </button>
+        </div>
+      </Transition>
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+defineEmits(["close-modal"]);
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+<style scoped>
+/* .modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+} */
+</style>
