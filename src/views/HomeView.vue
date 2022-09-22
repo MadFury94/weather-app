79ef6bf951d4aabffc6a87cfcb490f05b3ee5fdb
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-09-22 16:43:58 +0100

    Refactored BaseModal.vue
    Transitions and teleporting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const searchQuery = ref("");
+</script>
