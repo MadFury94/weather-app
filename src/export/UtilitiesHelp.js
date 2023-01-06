526d7e6937c05c456aecda13e118b61bc5beadf6
MadFury94 <onochieazukaeme@gmail.com>
2023-01-06 08:13:22 +0000

    added animated icons

--- src/export/UtilitiesHelp.js
+++ src/export/UtilitiesHelp.js
@@ -1,28 +1,26 @@
 export function return_my_icons(weatherType) {
-  console.log(weatherType);
   const my_weather_list = ["Rain", "Clouds", "Clear"];
-  //   return my_weather_list.includes(weatherType);
   if (my_weather_list.includes(weatherType)) {
-    console.log("i have the icon");
-    if (weatherType === "Clouds") {
-      return "cloudy.svg";
-    }
-    if (weatherType === "Clear") {
-      return "clear-day.svg";
-    }
-    if (weatherType === "Rain") {
-      return "raindrops.svg";
+    switch (weatherType) {
+      case "Clouds":
+        return "cloudy.svg";
+
+        break;
+      case "Clear":
+        return "clear-day.svg";
+        break;
+      case "Rain":
+        return "raindrops.svg";
+
+      case "Snow":
+        return "snowflake.svg";
+        break;
+
+      case "Mist":
+        return "mist.svg";
     }
   } else {
     console.log("i dont have the icon");
     return my_weather_list.includes(weatherType);
   }
-  //   console.log(my_weather_list.includes(weatherType));
-  //   if (weatherType === "Rain") {
-  //     console.log("Let rain show");
-  //   }
-  //   if (weatherType === "Clouds") {
-  //     return "cloudy.svg";
-  //   }
 }
-//  <img src="/images/cloudy.svg" alt="Animated cloudy weather" />
